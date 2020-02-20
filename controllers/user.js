fb06b0642d9e8b0630ab4eb3771060c7b78c9694
JCWBiles <jcbiles@outlook.com>
2020-02-20 11:46:10 +0000

    attempt 1

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,6 @@
 var users = require('../models/user');
 var bcrypt = require('bcrypt');
 var saltCount = 10;
-
-
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'student',
@@ -14,8 +12,6 @@ var UserController = {
   Index: function(req, res) {
     res.status(201).render('user/index');
   },
-
-
   Create: async (req, res) => {
     var { first_name, last_name, email, password } = req.body;
     let hashedPassword = await bcrypt.hash(password, 10)
@@ -27,52 +23,45 @@ var UserController = {
       if (error) {
         throw error
       }
-
-      // var api_key = 'YOUR API KEY';
-      // var domain = 'YOUR DOMAIN';
-      // var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
-      //
-      // var data = {
-      //   from: 'Excited User <your email>',
-      //   to: req.body.email,
-      //   subject: 'Hello',
-      //   text: 'Testing some Mailgun awesomeness!'
-      // };
-      //
-      // mailgun.messages().send(data, function (error, body) {
-      //   if (error){
-      //     console.log(error);
-      //   }
-      //   console.log(body);
-      // });
-
+    //   var api_key = 'YOUR API KEY';
+    //   var domain = 'YOUR DOMAIN';
+    //   var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+    //
+    //   var data = {
+    //     from: 'Excited User <your email>',
+    //     to: req.body.email,
+    //     subject: 'Hello',
+    //     text: 'Testing some Mailgun awesomeness!'
+    //   };
+    //
+    //   mailgun.messages().send(data, function (error, body) {
+    //     if (error){
+    //       console.log(error);
+    //     }
+    //     console.log(body);
+    //   });
+    //
       res.status(201).redirect('/films')
       // res.status(200).json(results.rows)
     })
   },
-
   Login: function(req, res) {
     res.status(201).render('login/index');
   },
-
   Authenticate: async function (req, res) {
     var  { email, password } = req.body;
-
     var foundUser = await pool.query(`SELECT * FROM users WHERE email = '${email}'`);
       console.log(email)
       var compare = await(bcrypt.compare(password, foundUser.rows[0]['password']))
           if (compare === true) {
             res.cookie('email', foundUser.rows[0]['email'])
-
             res.redirect('/films');
           }
           else {
             console.log('wrong password');
-
             res.status(201).redirect('/')
           }
   },
-
   Logout: function(req, res) {
     console.log(req.session.userId)
     req.session.destroy(function(err){
@@ -87,5 +76,4 @@ var UserController = {
     });
   }
 };
-
 module.exports = UserController;
