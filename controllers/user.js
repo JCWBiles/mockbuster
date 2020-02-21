19c8c39909ed9904a02fb8bfa9ade326d3ac6ea1
JCWBiles <jcbiles@outlook.com>
2020-02-21 17:12:12 +0000

    checkout added - still needs work

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 var users = require('../models/user');
 var bcrypt = require('bcrypt');
 var saltCount = 10;
+
 const Pool = require('pg').Pool
 const pool = new Pool({
   user: 'student',
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-
 var UserController = require('../controllers/user');
 
 router.get('/', UserController.Index);
