1901f9b9b6970d5ea02dd2dbd8625450413f8cf3
user <user@hoda003.home>
2020-03-26 18:43:04 +0000

    user can now update their password in account view

--- routes/account.js
+++ routes/account.js
@@ -9,6 +9,7 @@ router.post('/delete/:_id', UserController.Delete);
 router.post('/first/:_id', UserController.EditFirst);
 router.post('/last/:_id', UserController.EditLast);
 router.post('/email/:_id', UserController.EditEmail);
+router.post('/password/:_id', UserController.NewPassword);
 router.post('/upload/:_id', UserController.EditPic);
 router.get('/search', FilmsController.Search);
 router.get('/autocomplete', FilmsController.Autocomplete);
