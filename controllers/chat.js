d2c7b85d2be712b6fe083a477e215cb4b436d830
Rhodine Orleans-Lindsay <Rhodine.Orleans-Lindsay@digital.homeoffice.gov.uk>
2020-05-03 23:28:31 +0100

    working on im

--- controllers/chat.js
+++ controllers/chat.js
@@ -7,62 +7,20 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var ChatController = {
-  Index: function(req, res) {
-    var rooms = { Marvel: {} }
-    User.find({_id: req.session.userId}, function(err,users) {
-      if (err) { throw err; }
-      Films.find(function(err, films) {
-        if (err) { throw err };
-        Chat.find({},(err, chat)=> {
-          res.render('chat/index', { users: users, films: films, chat: chat, rooms: rooms });
-        })
-      })
-    })
-  },
-
-  CreateRoom: function(req, res){
-    var rooms = { Marvel: {} }
-    if (rooms[req.body.room] != null) {
-      return res.redirect('/chat')
-    }
-    rooms[req.body.room] = { users: {} }
-    res.redirect(req.body.room)
-    // Send message that new room was created
-    io.emit('room-created', req.body.room)
-    // res.redirect('/chat/room', { roomName: req.params.room })
-  },
-
-  Room: function(req, res){
-    // var rooms = { Marvel: {} }
-    // if (rooms[req.params.room] == null) {
-    //   return res.redirect('/chat')
-    // }
-    res.render('chat/room', { roomName: req.params.room })
-  },
-
-  Create: async (req, res) => {
-    try{
-      var message = new Chat(req.body);
-
-      var savedMessage = await message.save()
-      console.log('saved');
-
-      var censored = await Chat.findOne({message:'badword'});
-      if(censored)
-      await Chat.remove({_id: censored.id})
-      else
-      io.emit('message', req.body);
-      res.sendStatus(200);
-    }
-    catch (error){
-      res.sendStatus(500);
-      return console.log('error',error);
-    }
-    finally{
-      console.log('Message Posted')
-    }
-
-  }
+  // Index: function(req, res) {
+  //   var rooms = { Marvel: {} }
+  //   var chusers = {};
+  //   User.find({_id: req.session.userId}, function(err,users) {
+  //     if (err) { throw err; }
+  //     Films.find(function(err, films) {
+  //       if (err) { throw err };
+  //       Chat.find({},(err, chat)=> {
+  //         res.render('chat/index', { users: users, films: films, chat: chat, rooms: rooms });
+  //       })
+  //     })
+  //   })
+  // },
+  
 };
 
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,47 +1,57 @@
-const socket = io('http://localhost:3000')
-const messageContainer = document.getElementById('message-container')
-const roomContainer = document.getElementById('room-container')
-const messageForm = document.getElementById('send-container')
-const messageInput = document.getElementById('message-input')
+jQuery(function($){
+  var socket = io.connect('localhost:3001');
+  var $nickForm = $('#setNick');
+  var $nickError = $('#nickError');
+  var $nickBox = $('#nickname');
+  var $users = $('#users');
+  var $messageForm = $('#send-message');
+  var $messageBox = $('#message');
+  var $chat = $('#chat');
 
-if (messageForm != null) {
-  const name = prompt('What is your name?')
-  appendMessage('You joined')
-  socket.emit('new-user', roomName, name)
+  $nickForm.submit(function(e){
+    e.preventDefault();
+    socket.emit('new user', $nickBox.val(), function(data){
+      if(data) {
+        $('#nickWrap').hide();
+        $('#contentWrap').show();
+      }else {
+        $nickError.html('That username is already taken! Try again.');
+      }
+    });
+    $nickBox.val('');
+  });
 
-  messageForm.addEventListener('submit', e => {
-    e.preventDefault()
-    const message = messageInput.value
-    appendMessage(`You: ${message}`)
-    socket.emit('send-chat-message', roomName, message)
-    messageInput.value = ''
+  socket.on('usernames', function(data){
+    var html = '';
+    for (var i = 0; i < data.length; i++) {
+      html += data[i] + '<br>'
+    }
+    $users.html(html);
   })
+
+  $messageForm.submit(function(e){
+    e.preventDefault();
+    socket.emit('send message', $messageBox.val(), function(data){
+      $chat.append('<span class ="error">' + data + '</span><br>');
+    });
+    $messageBox.val('');
+  });
+  socket.on('load old msgs', function(docs){
+    for (var i = docs.length-1; i >= 0;  i--) {
+      displayMsg(docs[i]);
+    }
+  });
+
+  socket.on('new message', function(data){
+    displayMsg(data)[0].id;
+  });
+
+function displayMsg(data){
+    $chat.append('<span class ="msg"><b>' + data.name + ': </b>' + data.message + '</span><br>');
 }
 
-socket.on('room-created', room => {
-  const roomElement = document.createElement('div')
-  roomElement.innerText = room
-  const roomLink = document.createElement('a')
-  roomLink.href = `/${room}`
-  roomLink.innerText = 'Join'
-  roomContainer.append(roomElement)
-  roomContainer.append(roomLink)
-})
-
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`)
-})
-
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`)
-})
-
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`)
-})
-
-function appendMessage(message) {
-  const messageElement = document.createElement('div')
-  messageElement.innerText = message
-  messageContainer.append(messageElement)
-}
+  socket.on('private message', function(data){
+    // to add private message as a new div create a and append to different div
+    $chat.append('<span class ="private"><b>' + data.name + ': </b>' + data.message + '</span><br>');
+  })
+});
